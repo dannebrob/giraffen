f286a658e4d119fa24c94fbc9c89fba1612833e3
dannebrob <hej@danielbroback.se>
2023-12-04 08:09:19 +0100

    Refactor footer navigation links

--- src/components/footer/FooterNav.js
+++ src/components/footer/FooterNav.js
@@ -5,14 +5,14 @@ const FooterNav = () => {
   return (
     <div>
         <nav className='p-10 bg-sand flex flex-col'>
-            <h2 className='font-bold pb-2'>Länkar</h2>
-            <ul>
-            <li><Link href={'/lokalerna'}>Lokalerna</Link></li>
-            <li><Link href={'/kooperativ'}>Kooperativ</Link></li>
-            <li><Link href={'/mat'}>Maten</Link></li>
-            <li><Link href={'/personalen'}>Personalen</Link></li>
-            <li><Link href={'/jobb'}>Jobba hos oss</Link></li>
-            <li><Link href={'/kontakt'}>Kontakta oss</Link></li>
+            <h2 className='font-bold pb-2 md:hidden'>Länkar</h2>
+            <ul className='md:flex md:flex-wrap justify-center'>
+            <li className='px-2'><Link href={'/lokalerna'}>Lokalerna</Link></li>
+            <li className='px-2'><Link href={'/kooperativ'}>Kooperativ</Link></li>
+            <li className='px-2'><Link href={'/mat'}>Maten</Link></li>
+            <li className='px-2'><Link href={'/personalen'}>Personalen</Link></li>
+            <li className='px-2'><Link href={'/jobb'}>Jobba hos oss</Link></li>
+            <li className='px-2'><Link href={'/kontakt'}>Kontakta oss</Link></li>
             </ul>
 
 
