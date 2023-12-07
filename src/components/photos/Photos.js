df8f9600249ecf112badf621ceacb931447c0b92
dannebrob <hej@danielbroback.se>
2023-12-07 21:22:35 +0100

    Update photos in Photos component

--- src/components/photos/Photos.js
+++ src/components/photos/Photos.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import photo_1 from '@/assets/IMG_4.jpg'
-import photo_2 from '@/assets/IMG_8.jpg'
-import photo_3 from '@/assets/IMG_7.jpg'
 
 console.log('yes');
 export const Photos = () => {
   return (
     <div>
         <div className='flex justify-around flex-wrap py-5'>
-            <img alt='text' src={photo_1.src} className='max-w-md'/>
-            <img alt='text' src={photo_2.src} className='max-w-md'/>
-            <img alt='text' src={photo_3.src} className='max-w-md'/>
+            <img alt='text' src={'https://media.babyverden.no/wp-content/uploads/2013/03/28120937/crying-toddler-boy-picture-id1160272769.jpg'} className='max-w-md'/>
+            <img alt='text' src={'https://media.babyverden.no/wp-content/uploads/2013/03/28120937/crying-toddler-boy-picture-id1160272769.jpg'} className='max-w-md'/>
+            <img alt='text' src={'https://media.babyverden.no/wp-content/uploads/2013/03/28120937/crying-toddler-boy-picture-id1160272769.jpg'} className='max-w-md'/>
+           
         </div>
     </div>
   )
