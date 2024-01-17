f12534d2405e69a3f6bb42ae9b28e32d4e94ff73
dannebrob <hej@danielbroback.se>
2024-01-17 20:29:30 +0100

    Update pedagogik.js with changes to pedagogical activities and descriptions

--- src/pages/pedagogik.js
+++ src/pages/pedagogik.js
@@ -19,32 +19,65 @@ export default function Pedagogik() {
   return (
     <>
     <TopHead />
-    <main className='bg-sand flex flex-col md:flex-row md:justify-start items-center'>
+    <main className='bg-sand flex flex-col md:flex-row md:justify-start items-center py-10'>
       <div className='mx-10 md:w-1/2'>
         <h1 className={`text-center ${caveat.className} text-4xl py-10`}>Den pedagogiska verksamheten</h1>
         <p className='font-bold pb-4'>
         Ledorden omsorg, utveckling och lärande genomsyrar Förskolan Giraffen
         </p>
         <p className=' pb-4'>
-        Här möts vårdnadshavare, barn och kollegor av värme och engagemang
+        Vår vision är att varje barn ska få en lustfylld och lärorik utbildning, där trygghet, lärande och omsorg genomsyrar utbildningen och skapar förutsättningar för ett livslångt lärande. Vi värnar om det ekologiska och naturliga värderingarna som får genomsyra hela vår verksamhet.
         </p>
-        <p className='pb-4'>Vår pedagogiska verksamhet utgår från Skolverkets läroplan för förskolan. Förskolans pedagoger tar tillvara varje barns nyfikenhet och lust att lära. Leken är ett sätt att utvecklas, bearbeta intryck, kommunicera och inspireras. Hos oss är barnens inflytande en stor och viktig del där barns intresse visar vilken väg vi tillsammans ska jobba vidare på. Undervisningen anpassas aktivt efter det enskilda barnet samt barngruppen.
+        <p className='pb-4'>
+        Vår pedagogiska undervisning utgår från Skolverkets läroplan för förskolan samt Barnkonventionen. Förskolans pedagoger tar tillvara varje barns nyfikenhet och lust att lära. 
+        Hos oss är barnens inflytande en stor och viktig del där barns intresse visar vilken väg vi ska tar i vårt pedagogiska arbete.
         </p>
+        <p className='pb-4'>
+        Vi jobbar med olika teman och projekt där vi tar tillvara på barnens fantasi och idéer.
+        Undervisningar anpassas flytande och aktivt efter det enskilda barnet samt barngruppen.
+        </p>
+
         <h2 className={`${caveat.className} text-2xl py-5`}>Omsorg</h2>
-        <p>Vi på förskolan Giraffen anser att omsorg har en central del i hela vår verksamhet. Viktigt för oss är att alla känner sig trygga och att vi bygger upp nära relationer med barn och dess vårdnadshavare.
-        </p>
         <p>
-        I vardagsrutinerna som övergångar, påklädning och matsituationer finns omsorgen där genom uppmuntrande stöttning för att våga prova, för att lära och utmana sig. Men även i den fria leken är vi nära för att kunna möta upp barnens tankar och frågeställningar. Omsorg för oss är mer än bara att lyssna, det är även att kunna läsa av kroppsspråk och vara nära med kropp och själ. 
-        </p>
-        <p>
-        Och hur skulle det kunna se ut? Om ett barn en dag inte klarar att hålla sig vaken över måltiden,  kan vi spara lite mat så barnen får sitt när de vaknar. Eller om barnet har en känslomässigt tuff dag så är vi där och ger stöttning i den form som barnet söker, om det så är att sitta bredvid, bli sedd eller krypa upp i ett knä och bli kramad.
+        Vi på förskolan Giraffen anser att omsorg har en central del i hela vår verksamhet. 
+        Det viktig för oss att alla känner sig trygga, sedda och hörda och att vi bygger upp nära relationer med barn och dess vårdnadshavare. Genom en trygg grund kan lärande byggas vidare. Vi som personal ser det som vårt uppdrag att stötta barnen aktivt genom att vara tillgängliga, att lyssna, att vara inkännande, accepterande och närvarande.
         </p>
         <h2 className={`${caveat.className} text-2xl py-5`}>Utveckling</h2>
+        <p className='pb-4'>
+        På förskolan Giraffen har vi en helhetssyn på barns lärande och utveckling. Vårt pedagogiska arbete syftar till att uppmuntra och stötta barnen så att de känner en tilltro till sig själva och att de har en positiv uppfattning om sig själva.
+        </p>
+        <p className='pb-4'>
+        Vi vill möta och inspirera barnen i fantasin och därigenom stimulera deras utveckling och nyfikenhet att lära sig. Barns utveckling sker på olika sätt och vårt mål med vår verksamhet är att skapa en miljö där alla får plats och får det stödet som behövs. Vi fokuserar mycket på barnens språkutveckling, känslohantering och utveckling. Att lära sig att uttrycker sina känslor och behov är otroligt viktig för barnens utveckling.       
+        </p>
+        <p className='pb-4'>
+        Vi observerar barn och barngruppen, planerar, reflekterar och håller regelbundet kontakt med er som vårdnadshavare om barnens utveckling och hur vi kan samarbetet för att stödja barnen på bäst sätt.
+        </p>  
         <h2 className={`${caveat.className} text-2xl py-5`}>lärande</h2>
         <p className='pb-4'>
-        När förskollärarna planerar in våra lärsituationer har vi tagit tillvara barns intressen och nyfikenhet. Viktigt för oss är att det ska vara lärorikt och lustfyllt tillfälle när vi samlas i våra olika grupper. Dessa tillfällen kan se lite olika ut beroende på vad som är agendan. Här använder vi både innemiljön och utemiljön, i vår skog ges många möjligheter att utforska bland annat småkryp, se årstidsskiftningar och  motorisk träning. 
-
+        Vi som pedagoger planerar undervisningen efter barnens intressen och nyfikenhet. Läroplanens och Barnkonventionens mål är alltid med i fokus. Viktigt för oss är att det ska vara lärorikt och lekfullt.
         </p>
+        <p className='pb-4'>
+        Vi använder både innemiljön och utemiljön i vår undervisning.
+        </p>
+        <p className='pb-4'>
+        Innemiljön erbjuder många olika miljöer för att utforska och prova olika form av lek och samspel så som konstruktionsler och rollek. Det finns plats för pussel, spel, djur och massor med böcker.  Det finns en ateljémiljö för kreativitet, målande, skapande och pyssel.
+Även vårt kök är en del av vår lärmiljö, med hjälp av vår egen kock utforskar barnen sina sinnen genom mat och matlagning.
+        </p>
+        <p className='pb-4'>
+        I vår skog, som ligger precis utanför dörren ges många möjligheter till att lära och utforska allt från småkryp till kretslopp. Vi följer årstidsskiftningarna och lär oss tillsammans vi flyttar ut så mycket av vår undervisning som vi kan. I början av skogen finns det en rutschkana, lekstuga, en sandlåda och en balansgång. Vi går ofta på utflykt och promenader i skogen och till vår egna grillplats och strand där vi klättrar, kryper hoppar och springer.
+        </p>
+        <p>
+        Exempel på hur en vardag ser ut hos oss: 
+        </p>
+        <p>Frukost : 7.30-8:00</p>
+        <p>Lek : 8:00-9:00</p>
+        <p>Aktivitet i grupper/utelek : 9:00-10:45</p>
+        <p>Samling : 10:45-11:00</p>
+        <p>Lunch : 11.00-11:30</p>
+        <p>Vilan och lugn lek : 11:30- 12:30</p>
+        <p>Frilek inne eller ute : 12:30-14:00</p>
+        <p>Mellanmål : 14:00-14:15</p>
+        <p>Lek : 14:15 tills hämtat</p>
         </div>	
         <div className='md:w-1/2'>
       <Image
