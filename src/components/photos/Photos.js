e9d43878227ff865c10051270a0e71e3d2a97ada
dannebrob <hej@danielbroback.se>
2024-01-17 16:28:09 +0100

    Refactor photo layout with rounded corners

--- src/components/photos/Photos.js
+++ src/components/photos/Photos.js
@@ -10,9 +10,9 @@ export const Photos = () => {
   return (
     <div>
         <div className='flex justify-around flex-wrap py-5'>
-        <Image src={img_1} width={400} height={400} alt='text'/>
-        <Image src={img_2} width={400} height={400}  alt='text'/>
-        <Image src={img_3} width={400} height={400}  alt='text'/>
+        <Image src={img_1} width={400} height={400} alt='text' className='p-2 rounded-2xl'/>
+        <Image src={img_2} width={400} height={400}  alt='text' className='p-2 rounded-2xl'/>
+        <Image src={img_3} width={400} height={400}  alt='text' className='p-2 rounded-2xl'/>
         </div>
     </div>
   )
