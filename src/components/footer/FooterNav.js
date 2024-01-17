be08ce363872775906ced8e5f650b6cceb2dc0b6
dannebrob <hej@danielbroback.se>
2024-01-17 15:48:14 +0100

    Update footer navigation links

--- src/components/footer/FooterNav.js
+++ src/components/footer/FooterNav.js
@@ -7,12 +7,12 @@ const FooterNav = () => {
         <nav className='p-10 bg-sand flex flex-col'>
             <h2 className='font-bold pb-2 md:hidden'>Länkar</h2>
             <ul className='md:flex md:flex-wrap justify-center'>
-            <li className='px-2'><Link href={'/lokalerna'}>Lokalerna</Link></li>
-            <li className='px-2'><Link href={'/kooperativ'}>Kooperativ</Link></li>
-            <li className='px-2'><Link href={'/mat'}>Maten</Link></li>
-            <li className='px-2'><Link href={'/personalen'}>Personalen</Link></li>
-            <li className='px-2'><Link href={'/jobb'}>Jobba hos oss</Link></li>
-            <li className='px-2'><Link href={'/kontakt'}>Kontakta oss</Link></li>
+            <li className='px-2 hover:font-bold'><Link href={'/miljo'}>Ute- och innemijön</Link></li>
+            <li className='px-2 hover:font-bold'><Link href={'/kooperativ'}>Kooperativ</Link></li>
+            <li className='px-2 hover:font-bold'><Link href={'/mat'}>Maten</Link></li>
+            <li className='px-2 hover:font-bold'><Link href={'/personalen'}>Personalen</Link></li>
+            <li className='px-2 hover:font-bold'><Link href={'/jobb'}>Jobba hos oss</Link></li>
+            <li className='px-2 hover:font-bold'><Link href={'/kontakt'}>Kontakta oss</Link></li>
             </ul>
 
 
