fe38a8c541941da5a359bf8b72d12ede8108e51d
dannebrob <hej@danielbroback.se>
2024-02-20 20:15:26 +0100

    Refactor föräldrakooperativ.js to improve readability and clarify responsibilities

--- src/pages/kooperativ.js
+++ src/pages/kooperativ.js
@@ -21,42 +21,27 @@ export default function Kooperativ() {
       <div className='mx-10 md:w-1/2 py-10'>
         <h1 className={`text-center ${caveat.className} text-4xl py-10`}>Föräldrakooperativ</h1>
         <p>
-        Förskolan drivs som ett icke vinstdrivande föräldrakooperativ. Det innebär att föräldrarnas
-        ideella arbete och engagemang driver föreningen som är huvudman åt förskolan. Ansvaret för
-        den pedagogiska verksamheten och den dagliga driften är delegerat till förskolechefen. Den
-        insats som föräldrarna bidrar med är framför allt relaterad till lokalvård, inköp, och
-        styrelsearbete. Som förälder har du möjlighet att påverka och tycka till. Allt vi föräldrar gör
-        bidrar till en fin och trygg miljö för våra barn, vilket är både roligt och givande.
+        Förskolan drivs av ett icke vinstdrivande föräldrakooperativ, vilket innebär att det är vi vårdnadshavare som 
+        tillsammans är huvudmän och arbetsgivare. Ansvaret för den pedagogiska verksamheten och den dagliga driften är 
+        delegerat till förskolans rektor.
         </p>
         <p className='py-4'>
-        Föräldraengagemanget är det som gör vår förskola så speciell. Allt som vi föräldrar bidrar med
-        i föreningen i form av tid och engagemang går direkt tillbaka till barnen. Utan föräldrarna
-        skulle vi inte kunna ha så bra verksamhet som vi har idag.        </p>
-        <h2 className={`${caveat.className} text-2xl py-5`}>Styrelse</h2>
-        <p>
-        Föräldraföreningens styrelse är arbetsgivare och har ansvaret för verksamheten. Här ingår en
-        ordförande, sekreterare, kassör, löneansvarig och personalansvarig samt två suppleanter som
-        hjälper till vid behov. På styrelsens ansvar ligger budgetarbete, kommunikation med kommun,
-        arbetsgivaransvar med mera.
+        Föräldraengagemenaget är det som gör vår förskola så speciell. Allt som vi vårdnadshavare bidrar med i föreningen 
+        i form av tid och engagemang går direkt tillbaka till barnen. Tillsammans skapar vi förutsättningar för en trygg 
+        och utvecklande förskola för våra barn! Just att vi gör det tillsammans skapar också en positiv gemenskap för oss 
+        vårdnadshavare.
         </p>
-        <h2 className={`${caveat.className} text-2xl py-5`}>Arbetsgrupper</h2>
         <p>
-        När man blir medlem i föreningen blir man också tillfrågad vilken arbetsgrupp man vill tillhöra.
-        För att förskolan ska fungera på bästa sätt finns arbetsgrupperna: Innegruppen, Utegruppen,
-        Trivselgruppen, Inköp och IT, där varje vårdnadshavare hjälper till med sådant som behöver
-        göras under årets gång utöver styrelsens arbete.        </p>
-        <h2 className={`${caveat.className} text-2xl py-5`}>Arbetsdagar</h2>
-        <p>
-        Två gånger om året utförs arbetsdagar, en gång på våren och en gång på hösten. På
-        arbetsdagarna städar vi mer grundligt både inne och ute, lagar och byter ut sådant som gått
-        sönder och ser till att miljön är fräsch och fin i allmänhet. Här drar alla föräldrar sitt strå till
-        stacken samtidigt som vi spenderar tid tillsammans, äter, fikar och lär känna varandra bättre.
-        </p> 
-
-        <h2 className={`${caveat.className} text-2xl py-5`}>Städ</h2>
-        <p>
-        Den huvudsakliga städningen i Giraffens lokaler sköts av oss vårdnadshavare, tisdagar och torsdagar efter stängning. Varje förälder tilldelas ett antal pass per termin enligt ett rullande schema. Ett visst dagligt städ sköts även av personalen.  
+        I praktiken innebär det att vi vårdnadshavare i första hand driver styrelsearbetet. Det kan kännas läskigt och 
+        låta jobbigt att vara med i en styrelse, särskilt om det är första gången. Men detta är någonting vi gör tillsammans 
+        som dessutom ger väldigt god erfarenhet. Styrelsen utgörs av en ordförande, sekreterare, kassör, löneansvarig och 
+        personalansvarig. De vårdnadshavare som inte är med i styrelsen bidrar genom mindre insatser under året, såsom att 
+        reparera en trasig uteleksak, fixa fika till arbetsdagar och liknande.
         </p>
+        <p>
+        Den huvudsakliga städningen sköts också av oss med hjälp av vår robotdammsugare! Varje vårdnadshavare tilldelas ett antal 
+        pass per termin enligt ett rullande schema. Två gånger per år ses vi allihop för arbetsdag och gör extra fint på vår förskola.
+       </p>
         </div>	
         <div className='md:w-1/2'>
       <Image
