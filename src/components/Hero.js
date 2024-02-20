54ecb4ba2f929426f1c8d3d2726ed62b4f1cd29d
dannebrob <hej@danielbroback.se>
2024-02-20 21:18:18 +0100

    Refactor text in Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -25,25 +25,31 @@ export const Hero = () => {
             <div className='flex flex-col px-8 md:w-1/2'>
             <h1 className={`${caveat.className} text-5xl py-8 text-center`}>Om Giraffen</h1>
             
-            <p className>
-            Hos oss finns tiden för omsorg om barn. Genom våra pedagogers och vårdnadshavares
-            närvaro och engagemang för vår relativt lilla grupp barn ger vi barnen goda förutsättningar
-            att känna sig trygga, sedda och inspirerade i den miljö de befinner sig under dagarna.
-            Dessutom är vår utemiljö otroligt vacker och våra omgivningar inspirerande i sig!
-
+            <p>
+            Vi har tid för omsorg om våra barn. Genom våra pedagogers och vårdnadshavares närvaro och 
+            engagemang för barnen får de goda förutsättningar att känna sig trygga och inspirerade i 
+            den miljö de befinner sig under dagarna. Dessutom är vår utemiljö otroligt vacker och 
+            våra omgivningar inspirerande i sig!
             </p>
-            <div className=''>
-            <p className='pt-4 font-bold text-start'>Hos Giraffen får du:</p>
+            <p className='py-2'>
+            Vi har en åldersintegrerad barngrupp (1–5 år) vilket ger barnen många möjligheter att 
+            lära sig av varandra. Tillsammans med pedagogerna skapar de en väldigt dynamisk och 
+            naturlig grupp vilket främjar utveckling och sociala färdigheter. Läromiljön anpassas 
+            också efter ålder och mognad genom indelningar för specifika aktiviteter och temaarbete. 
+            Det sker i mindre grupper vilket främjar möjligheten till koncentration och förmågan att fokusera.
+            </p>
+            <div className={`${caveat.className}`}>
+            <p className='pt-4 font-bold text-start'>På Giraffen har vi:</p>
             <ul className='text-start'>
-                <li>• Liten barngrupp</li>
-                <li>• Hög personaltäthet</li>
-                <li>• Kvalitetssäkrat pedagogiskt arbete</li>
-                <li>• Fantastisk utemiljö</li>
-                <li>• Mat som lagas på plats</li>
-                <li>• Stort engagemang för barnen</li>
-                <li>• Genomsyrande gemenskap och delaktighet</li>
-                <li>• Insyn i verksamheten</li>
-                <li>• Miljö- och hållbarhetstänk</li>
+                <li>● Liten barngrupp</li>
+                <li>● Hög personaltäthet</li>
+                <li>● Kvalitetssäkrat pedagogiskt arbete</li>
+                <li>● Fantastisk utemiljö</li>
+                <li>● Mat som lagas på plats</li>
+                <li>● Stort engagemang för barnen</li>
+                <li>● Genomsyrande gemenskap och delaktighet</li>
+                <li>● Insyn i verksamheten</li>
+                <li>● Miljö- och hållbarhetstänk</li>
             </ul>
             </div>
             <Link href="/kontakt" className={`p-2 bg-green w-52 my-3 text-center rounded-lg text-2xl text-white ${caveat.className}`}>Ställ ditt barn i kö</Link>
