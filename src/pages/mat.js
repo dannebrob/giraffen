59346d9da0ac6aa14f7836d4feb70712187a6d56
dannebrob <hej@danielbroback.se>
2024-02-20 20:23:29 +0100

    Refactor Mat component and update food description

--- src/pages/mat.js
+++ src/pages/mat.js
@@ -23,15 +23,17 @@ export default function Mat() {
       <main className='bg-sand flex flex-col md:flex-row md:justify-start '>
         <div className='mx-10 md:w-1/2 py-10'>
           <h1 className={`text-center ${caveat.className} text-4xl py-10`}>Mat</h1>
-          <p className='pb-4'>Giraffen har en egen kock som lagar mat från grunden och i möjligaste mån med ekologiska och närproducerade råvaror. Under sommarhalvåret har vi lyxen att få grönsaksleverans från Hemsjö köksträdgård. Veckomenyn varierar, det bjuds på fisk minst en gång i veckan, matiga soppor, husmanskost och olika vegetariska rätter.
+          <p className='pb-4'>
+          Giraffen har en egen kock som lagar mat från grunden och i möjligaste mån med 
+          ekologiska och närproducerade råvaror. Under sommarhalvåret har vi lyxen att få 
+          grönsaksleverans från Hemsjö köksträdgård. Veckomenyn varierar, det bjuds på fisk 
+          minst en gång i veckan, matiga soppor, husmanskost och olika vegetariska rätter. 
+          Specialkost för de barn som behöver eller önskar det är en självklarhet.
           </p>
           <p className='pb-4'>
-          Maten är även en del av den pedagogiska verksamheten. Barnen på förskolan är ofta välkomna in i köket där de deltar i förberedelser, tillagning av lunch och mellanmål. Med detta skapas en nyfikenhet och förståelse för matens ursprung och betydelse.
-          </p>
-          <h2 className={`${caveat.className} text-2xl py-5`}>Kommentar från Andrew</h2>
-
-          <p>Maten är även en del av den pedagogiska verksamheten. Barnen på förskolan är välkomna in i köket och delta i förberedelser, tillagning av lunch och mellanmål. 
-            Med detta skapas en nyfikenhet och förståelse för matens ursprung och betydelse.
+          Maten är även en del av den pedagogiska verksamheten. Barnen på förskolan är välkomna 
+          in i köket och deltar i förberedelser, tillagning av lunch och mellanmål. 
+          Med detta skapas en nyfikenhet och förståelse för matens ursprung och betydelse.
           </p>
           </div>	
           <div className='md:w-1/2'>
