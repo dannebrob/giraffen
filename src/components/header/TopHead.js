669afad87ba5abb83d756e062f355421b0695768
Daniel Brobäck <65211641+dannebrob@users.noreply.github.com>
2025-08-11 14:36:49 +0200

    Update TopHead.js
    fixed name of links in nav

--- src/components/header/TopHead.js
+++ src/components/header/TopHead.js
@@ -22,26 +22,32 @@ const pathname = usePathname()
     {
       id: 1,
       link: "miljo",
+      name: "miljö,
     },
     {
       id: 2,
       link: "kooperativ",
+      name: "kooperativ",
     },
     {
       id: 3,
       link: "pedagogik",
+      name: ""pedagogik",
     },
     {
       id: 4,
       link: "personalen",
+      name: "personalen"
     },
     {
       id: 5,
       link: "mat",
+      name: "mat"
     },
     {
       id: 6,
       link: "kontakt",
+      name: "kontakt"
     },
 
   ];
@@ -58,12 +64,12 @@ const pathname = usePathname()
 
       <ul className="hidden md:flex items-center">
       
-        {links.map(({ id, link }) => (
+        {links.map(({ id, link, name }) => (
           <li
             key={id}
             className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
           >
-            <Link href={link} className={pathname === '/' + link ? 'font-bold' : ''}>{link}</Link>
+            <Link href={link} className={pathname === '/' + link ? 'font-bold' : ''}>{name}</Link>
           </li>
         ))}
         <Link href="/kontakt" className={`bg-green rounded-md p-4 text-white ${caveat.className}`}>Ställ ditt barn i kö</Link>
@@ -79,13 +85,13 @@ const pathname = usePathname()
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-greenDark from-black to-gray-800 text-gray-500">
            <Link href="/kontakt" className={`bg-green rounded-md p-4 text-white ${caveat.className}`}>Ställ ditt barn i kö</Link>
-          {links.map(({ id, link }) => (
+          {links.map(({ id, link, name }) => (
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
               <Link onClick={() => setNav(!nav)} href={link}>
-                {link}
+                {name}
               </Link>
             </li>
           ))}
